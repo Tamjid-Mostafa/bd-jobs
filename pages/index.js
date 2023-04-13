fb7cc558762c70841e5cb09ef3dcae53c14df768
Tamjid Mostafa <tamjid430@gmail.com>
2023-04-13 16:30:21 +0600

    update favicon

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Category from "@components/Category";
 import Countries from "@components/Countries";
 import Hero from "@components/Hero";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 import Link from "next/link";
 import { FaArrowAltCircleUp } from "react-icons/fa";
 
@@ -11,6 +12,11 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <div className="relative">
+      <Head>
+        <title>Largest Overseas Job Site in Bangladesh | Bdjobs.com</title>
+        <link rel="shortcut icon" href="/favicon.png" />
+        <meta property="og:title" content="My page title" key="title" />
+      </Head>
       <Hero />
       <Category />
       <Countries />
